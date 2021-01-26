9216466e8c87d84dc1a61b3af2c4f0992065805e
BeiZhang <bei.zhang@mujin.co.jp>
2021-01-26 15:57:00 +0800

    summary ways of two sum fast ways

--- src/src/ThreeSum/TwoSumFast.java
+++ src/src/ThreeSum/TwoSumFast.java
@@ -1,5 +1,14 @@
-//reference https://www.cnblogs.com/evasean/p/7208900.html
+//reference Link
+// https://www.cnblogs.com/evasean/p/7208900.html
 // https://shadowoom123.wordpress.com/2018/04/05/3-sum-in-quadratic-time/
+// https://web.stanford.edu/class/cs9/sample_probs/TwoSum.pdf
+
+// there are three ways
+// 1- brute force
+// 2- sorting and binary search
+// 3- hashing
+
+
 public class TwoSumFast {
 
 }
